19271eeb2d9ec11163c88d9b5acfdb2d273ec1cc
Renan Perisse <renanperissedeoliveira@gmail.com>
2023-06-14 12:55:47 -0300

    Payment

--- src/main/java/com/example/practice/config/TestConfig.java
+++ src/main/java/com/example/practice/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.example.practice.entities.Category;
 import com.example.practice.entities.Order;
 import com.example.practice.entities.OrderItem;
+import com.example.practice.entities.Payment;
 import com.example.practice.entities.Product;
 import com.example.practice.entities.User;
 import com.example.practice.entities.enums.OrderStatus;
@@ -80,5 +81,11 @@ public class TestConfig implements CommandLineRunner {
 		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice()); 
 
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
+		
+		Payment pay1 = new Payment(null, Instant.parse("2023-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
+		
 	}
 }
